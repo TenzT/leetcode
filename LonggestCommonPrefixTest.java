36e75b60cebda14c40a416aa8d55478a13c75fe0
Tenz <tenz@sina.cn>
2018-03-22 21:48:37 +0800

    LonggestCommonPrefixTest.java

---
+++ LonggestCommonPrefixTest.java
@@ -0,0 +1,26 @@
+package Easy;
+
+public class LonggestCommonPrefixTest {
+	public static String LonggestCommonPrefix(String[] strs) {
+		if (strs.length == 0) {
+			return "";
+		}
+		String prefix = strs[0];
+		for(int i = 1; i <strs.length; i++) {
+			while(strs[i].indexOf(prefix) != 0) {
+				prefix = prefix.substring(0, prefix.length()-1);
+				if(prefix.isEmpty()) {
+					return "";
+				}
+			}
+		}
+		
+		return prefix;
+	}
+	
+	public static void main(String[] args) {
+		String[] strs = {"123", "121", "12465"};
+		String res = LonggestCommonPrefix(strs);
+		System.out.println(res);
+	}
+}
